9e1a26a62bdcb2a6269eeb9b4075270d1dc96ce7
Laura Latuillerie <laura.latuillerie@gmail.com>
2022-10-14 20:39:21 +0200

    Add static from previous portfolio & apply new brand identity

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+window.onload = function () {
+  document.getElementById("loading").style.display = "none";
+};
+
+new TypeIt(".typing-effect", {
+  strings: "",
+  speed: 120,
+  loop: true,
+})
+  .type("Hello !", { delay: 100 })
+  .delete(18, { delay: 800 })
+  .type("私はローラです", { delay: 100 })
+  .delete(7, { delay: 800 })
+  .type("ᕦ(ò_óˇ)ᕤ", { delay: 100 })
+  .delete(8, { delay: 800 })
+  .go();
